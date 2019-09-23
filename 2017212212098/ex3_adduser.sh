e8475a7b7005fe876cb4b0c91b97f3d7fe52bb67
kqzh <vxj555@qq.com>
2019-09-23 10:09:30 +0800

    commit ex3

---
+++ 2017212212098/ex3_adduser.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+Usage() {
+
+	echo -e "\nUsage: ${0} [-l LENGTH] [-s SPECIAL_NUM] [-o IS_SHUF]\n" >&2
+	
+	echo -e 'create a new user\n'
+	
+	echo -e '-l LENGTH specify the password length\n'
+	
+	echo -e '-s LENGTH specify the special char length\n'
+	
+	echo -e '-o Judge whether to shuf password or not\n'
+	
+	exit 1
+}
+
+LENGTH=8
+
+SPECIAL_NUM=2
+
+FLAG=false
+
+USER_NAME=${!#}
+
+while getopts ol:s: OPTION
+do 
+	case "${OPTION}" in
+
+	l)
+		LENGTH=${OPTARG}
+		;;
+	s)
+		SPECIAL_NUM=${OPTARG}
+		;;
+	o)
+		FLAG=true
+		;;
+
+	?)
+		Usage
+		;;
+	esac
+done
+
+CHAR_NUM=$((${LENGTH}-${SPECIAL_NUM}))
+
+PASS=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c${CHAR_NUM})
+
+for((i=0;i<${SPECIAL_NUM};i++))
+do
+	SPECIAL=$(echo '!@#$%^&*()_+' | fold -w1 | shuf | head -c1)
+
+	WORD=${WORD}${SPECIAL}
+done
+
+PASSWORD=${PASS}${WORD}
+
+if ${FLAG}
+	then PASSWORD=$(echo ${PASSWORD} | fold -w1 | shuf| tr -d '\n')
+fi
+
+echo ${PASSWORD}
+
+useradd -c "${USER_NAME}" -m ${USER_NAME}
+
+echo ${USER_NAME}:${PASSWORD} | chpasswd
+
+#passwd -e ${USER_NAME}
