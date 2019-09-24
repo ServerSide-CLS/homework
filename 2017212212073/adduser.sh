9a81dde5a0ec889be272b5e9a1a0e9577ae99fee
CyrilPycc <50103042+CyrilPycc@users.noreply.github.com>
2019-09-24 22:12:03 +0800

    上传了实验二

--- 2017212212073/adduser.sh
+++ 2017212212073/adduser.sh
@@ -1,57 +1,21 @@
 #!/bin/bash
 
-usage() {
-  echo "Usage: ${0} [-l LENGTH] [-s special char] [-o ORDER]" >&2
-  echo 'Generate a random password'
-  echo '  -l LENGTH specify the password length' 
-  echo '  -s Append special char to the password'
-  echo '  -o ORDER disorder the password'
-  exit 1
-}
+read -p 'Enter username:' USER_NAME
+read -p 'Enter person info:' COMMENT
 
-if [[ "${USE_SPEC_CHAR}" = 'true' ]]
-then
-  LEN=`expr ${LEN1} - ${CH}`
-  PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c${LEN})
-  SPEC_CHAR=$(echo '!@#$%^&*()_+=' | fold -w${CH} | shuf | head -c${CH})
-  PASSWORD="${PASSWORD}${SPEC_CHAR}"
-fi
+NUM="${RANDOM}${RANDOM}"
+PN=$(echo "${NUM}" | head -c6)
+echo "${PN}"
+CH=$(echo '!@#$%^&*()_+=' | shuf | head -c2)
+echo "${CH}"
+PASSWORD="${PN}${CH}"
+PA=$(echo "${PASSWORD}" | fold -w1 | shuf | head -c1 )
+echo "${PA}"
 
-if [[ "${PWA_ORDER}" = 'true' ]]
-then
-  LEN=`expr ${LEN1} - ${CH}`
-  PASSWORD=$(exho ${PASSWORD} | fold -w1 | shuf | tr -d '\n' | head -c${LEN1})
-fi
+useradd -c "${COMMENT}" -m "${USER_NAME}"
 
-LEN=8
-PAW_ORDER='true'
+echo "your password : $PASSWORD"
 
-ARRAY=(${@})
-NUM=${#}
-USER_NAME=${ARRAY[$NUM-1]}
+echo ${USER_NAME}:${PASSWORD}|chpasswd
 
-while getopts l:s:o OPTION
-do
-  case "${OPTION}" in
-    l)   
-      LEN1="${OPTARG}"
-      ;;
-    s)   
-      CH="${OPTARG}"
-      ;;
-    o)
-      PAW_ORDER='false'
-      ;;
-    ?)
-      echo 'Invalid option' >&2 ; 
-      usage
-      ;;
-  esac
-done
-
-useradd -c "${COMMENT}"  -m "${USER_NAME}"
-
-echo "your password : ${PASSWORD}"
-
-echo ${USERNAME}:${PASSWORD}|chpasswd
-passwd -e ${USERNAME}
+passwd -e ${USER_NAME}
