811138655a93747b7930d0c9fac15c8e973cb84d
2017212212183 <41031967+2017212212183@users.noreply.github.com>
2019-10-15 21:00:42 +0800

    add lab5

---
+++ 2017212212183/server.js
@@ -0,0 +1,17 @@
+//请求（request）Node.js自带的http模块，并把它赋值给http变量
+var http = require('http');
+
+//调用http模块自带的函数，createServer,会返回 一个对象，这个对象有一个叫做 listen 的方法，这个方法有一个数值参数， 指定这个 HTTP 服务器监听的端口号。 
+http.createServer(function (request, response) {
+
+    // 发送 HTTP 头部 
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
