bf0e3fa7c2a5f8fd2132f5dcd84c3bf8de6716d4
Yu Cheng <49930404+yuch256@users.noreply.github.com>
2019-11-05 23:48:31 +0800

    Update index.js

--- 2017212212230/ex07_express-hbs/routes/index.js
+++ 2017212212230/ex07_express-hbs/routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const entries = Array.from(new Array(68), (v, k) => k)
+  const entries = Array.from(new Array(68), (v, k) => k)                // all data
   const count = entries.length
-  const pageSize = 8
-  const pageCount = Math.ceil(count / pageSize)
-  const page = req.query.page || 1
-  const data = entries.slice((page - 1) * pageSize, page * pageSize)
+  const pageSize = 8                                                    // page size
+  const pageCount = Math.ceil(count / pageSize)                         // total page
+  const page = req.query.page || 1                                      // current page
+  const data = entries.slice((page - 1) * pageSize, page * pageSize)    // current page datas
   const pageCountList = Array.from(new Array(pageCount), (v, k) => {
     return {
       value: k + 1,
