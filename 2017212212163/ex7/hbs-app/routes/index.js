1165c843394ae2b9f2d9a354ff34fe086a200fcb
MAllstar <46458404+MAllstar@users.noreply.github.com>
2019-11-06 00:28:39 +0800

    Update index.js

--- 2017212212163/ex7/hbs-app/routes/index.js
+++ 2017212212163/ex7/hbs-app/routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 /* GET home page. */
 //创建路由和渲染模板
 router.get('/', function(req, res, next) {
-  const count = 23;
+  const count = 40;
   const pageId = req.query.page || 1;  // 获取链接参数  
-  const pageSize = 6;  //页规模
+  const pageSize = 10;  //页规模
   const pageCount = Math.ceil(count/pageSize);  //页数
   const pageList = Array.from(Array(pageCount),(v,i) => i+1)  //页列表,Array.from()将其他对象转换为数组。
-  const dataArray = Array.from(Array(6), (v,i)=>i + 6*pageId - 5);
+  const dataArray = Array.from(Array(10), (v,i)=>i + 10*pageId - 9);
   res.render('home', {
     layout: 'default', 
     params: 'm-home', 
@@ -18,11 +18,6 @@ router.get('/', function(req, res, next) {
     pageList,
     dataArray,
     count,
-    people: [
-      {firstName: "Yehuda", lastName: "Katz"},
-      {firstName: "Carl", lastName: "Lerche"},
-      {firstName: "Alan", lastName: "Johnson"}
-    ]
   });
 });
 
