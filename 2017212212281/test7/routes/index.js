b6b4d53238a6502661cc8496657f5924ccfa4c09
ItsHeart <55654863+ItsHeart@users.noreply.github.com>
2019-11-06 00:01:03 +0800

    Update index.js

--- 2017212212281/test7/routes/index.js
+++ 2017212212281/test7/routes/index.js
@@ -1,20 +1,18 @@
-  
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const items = Array.from(Array(70), (v, k) => k)
+  const items = Array.from(Array(51), (v, k) => k)
   const count = items.length
-  const pageSize = 8
-  const pageCount = Math.ceil(count / pageSize)
-  const page = req.query.page || 1
-  const data = items.slice((page - 1) * pageSize, page * pageSize)
-  const pageCountList = Array.from(Array(pageCount), (v, k) => k + 1)
+  const pages = 8
+  const pageNum = Math.ceil(count / pages)
+  const pageNow = req.query.page || 1
+  const data = items.slice((pageNow - 1) * pages, pageNow * pages)
+  const pageCountList = Array.from(Array(pageNum), (v, k) => k + 1)
   res.render('index', {
     layout: 'default',
-    page: page,
-    pageCount: pageCount,
+    pageNum: pageNum,
     pageCountList: pageCountList,
     data: data,
   });
