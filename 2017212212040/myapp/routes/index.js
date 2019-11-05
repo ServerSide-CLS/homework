ff2301357d9cfd487fd3e7374212fbc02c50fb49
fufuuuu <40791664+fufuuuu@users.noreply.github.com>
2019-11-05 21:21:23 +0800

    uploaded exp7

---
+++ 2017212212040/myapp/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    const entries = Array.from(Array(60), (v, k) => k + 1)
+    const pageSize = 8
+    const pageCount = 7
+    const page = req.query.page || 1
+    const data = entries.slice((page - 1) * pageSize, page * pageSize)
+    const pageCountList = Array.from(Array(pageCount), (v, k) => k + 1)
+    res.render('index', {
+      layout: 'default',
+      page: page,
+      pageCount: pageCount,
+      pageCountList: pageCountList,
+      data: data,
+    });
+});
+  
+module.exports = router;
