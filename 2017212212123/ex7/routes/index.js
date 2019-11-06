3d839a2c4c6810cabd53b680c2ec664419fb607f
Yhwang123 <55697208+Yhwang123@users.noreply.github.com>
2019-11-06 09:15:44 +0800

    Update index.js

--- 2017212212123/ex7/routes/index.js
+++ 2017212212123/ex7/routes/index.js
@@ -5,18 +5,18 @@ var data = require('../data');
 router.get('/', function(req, res, next) {
   page = req.query.page || 1;
   module.exports = page;
-  datas = "{\"photo\":[";
-  data.photo.forEach(function(x){
-    if (x.id < page * 6 && x.id >= (page - 1) * 6)
-      datas += "{ \"image\": \"" + x.image + "\"},";
+
+  var datas = "{\"message\":[";
+  data.message.forEach(function(x){
+    if (x.id < page * 8 && x.id >= (page - 1) * 8)
+      datas += "{ \"text\": \"" + x.text + "\"},";
+      // console.log(x);
   });
   datas += "]}";
   datas = eval('(' + datas + ')');
 
   page = 2;
-
-  res.render('home', {layout: 'default', photo: datas.photo, l1: parseInt(page) - 1, l2: parseInt(page)});
-
+  res.render('home', {layout: 'default', messages: datas.message, l1: parseInt(page) - 1, l2: parseInt(page)});
 });
 
 module.exports = router;
