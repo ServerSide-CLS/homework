7775548c8d2ac6574c4d8310c155bdc88300f207
SmileFishdog <48762874+SmileFishdog@users.noreply.github.com>
2019-11-12 15:48:10 +0800

    upload ex8（刚刚交错工程了）

--- 2017212212167/homework8/app.js
+++ 2017212212167/homework8/app.js
@@ -35,6 +35,14 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+/*app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    next();
+  });*/
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- 2017212212167/homework8/routes/index.js
+++ 2017212212167/homework8/routes/index.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var nodemailer = require('nodemailer');
 var fsPath = "./user.json";
 var ckCode="";
+var lastEmail="";
+var reg = /^[0-9a-zA-Z_.-]+[@][0-9a-zA-Z_.-]+([.][a-zA-Z]+){1,2}$/;
 
 var transporter = nodemailer.createTransport({
     host: 'smtp.163.com', 
@@ -15,35 +17,57 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-
 router.get('/', function(req, res){
 	res.render('index');
-	for(var i=0;i<6;i++)
-	{
-	    ckCode+=Math.floor(Math.random()*10);
-	}
 });
 
 router.post('/sendCode',function(req, res)
 {
-	console.log(req.body);
-	var address=req.body.email;
-    	var mailObj = {
-		    from: 'duoduobao@163.com', // 发送方邮箱及标题
-		    to: address, // 对方邮箱地址
-		    subject: '验证码来啦！', // 
-		    text: ckCode, // 邮件内容
+	lastEmail=req.body.email;
+	if(reg.test(req.body.email))
+	{
+		ckCode="";
+		for(var i=0;i<6;i++)
+		{
+		    ckCode+=Math.floor(Math.random()*10);
+		}
+		var address=req.body.email;
+	    var mailObj = {
+			from: 'duoduobao@163.com', // 发送方邮箱及标题
+			to: address, // 对方邮箱地址
+			subject: '验证码来啦！', // 
+			text: ckCode, // 邮件内容
 		};
 		transporter.sendMail(mailObj);
+		res.send("验证码邮件发送成功，请登录邮箱查看验证码>.<");
+	}
+	else
+	{
+		res.send(req.body.email+"请输入正确的邮箱地址！");
+	}
 });
 
 router.post('/', function(req, res){
-    var str = JSON.stringify(req.body,"","\t");
- 
-    //}
-    fs.writeFile('user.json',str,function(err){
-		if (err) {res.status(500).send('Server is error...')}
-	})
+ 	if(req.body.email!=lastEmail)//邮箱不一致
+ 	{
+ 		res.send("邮箱不一致！");
+ 	}
+ 	else if(req.body.checkcode!=ckCode)//验证码不正确
+ 	{
+ 		res.send("验证码不正确！");
+ 	}
+ 	else if(req.body.password!=req.body.comfirm)//两次密码不一致
+ 	{
+ 		res.send("两次密码不一致！");
+ 	}
+ 	else
+ 	{
+ 		var str = JSON.stringify(req.body,"","\t");
+ 		fs.writeFile('user.json',str,function(err){
+			if (err) {res.status(500).send('Server is error...')}
+		})
+		res.send("注册成功(*^▽^*)！");
+ 	}
 });
 
 module.exports = router;
