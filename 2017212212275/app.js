c94319f8e47f4d5ff5ae0b254218bbebefd475ae
wykang0720 <55696829+wykang0720@users.noreply.github.com>
2019-11-16 22:10:45 +0800

    Add files via upload

---
+++ 2017212212275/app.js
@@ -0,0 +1,64 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var hbs = require('express-handlebars');
+const Handlebars = require('handlebars')
+
+var indexRouter = require('./routes/index');
+
+var static = require('express-static')
+var app = express();
+
+
+
+
+app.set('view engine', 'hbs')
+// 设置静态资源目录
+app.set('views', path.join(__dirname, 'views'))
+
+
+console.log('端口号为3000')
+
+// 设置handlebars参数
+app.engine( '.hbs', hbs( {
+  extname: '.hbs',
+  defaultLayout: 'default',
+  layoutsDir:  __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/',
+}));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+ 
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.use(express.static('public'));
+module.exports = app;
+
+app.listen(3000)
