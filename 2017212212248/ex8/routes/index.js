d59facaf455922e41a0c6105f51397a34e45219f
plwdxiiu <43988932+plwdxiiu@users.noreply.github.com>
2019-11-18 21:17:54 +0800

    Update index.js

--- 2017212212248/ex8/routes/index.js
+++ 2017212212248/ex8/routes/index.js
@@ -7,13 +7,16 @@ var fs = require("fs");
 router.get('/', function(req, res, next) {
 
     res.render('index', {
-        title: '注册页'
+        title: '注册页',
+        mytitle:'注册'
     });
 });
 
 router.get('/email/:email', (req, res) =>{
     let email = req.params.email;
     let code =  ('0000' + Math.floor(Math.random() * 999999)).slice(-4);
+    req.session.code = code;
+    req.session.save();
 
     //配置邮箱
     let transporter = nodemailer.createTransport({
@@ -37,28 +40,59 @@ router.get('/email/:email', (req, res) =>{
 
     transporter.sendMail(mailOptions,function (error,info) {
         if(error){
-            res.send({status:"fail"})
+            res.send({status: "fail"});
+        }
+        else{
+            res.send({status: "success"});
         }
         //验证码发送失败
     });
 
 });
 
+//注册
+console.log(111111);
 router.post('/register',(req,res) => {
     let info = req.body;
     console.log(info);
+
     //验证码校验
-    if(info.code == code){
-        let user = {
-            email: info.email,
-            password: info.passwd,
-        };
+    if(info.code == req.session.code){
         let data = JSON.parse(fs.readFileSync("user.json"));
-        data.push(user);
-        fs.writeFileSync("user.json",JSON.stringify(data));
-        res.send({status:"success"});
+        let loginFlag = true;
+        console.log(data);
+
+        //判断是否已注册
+        for(let i = 0; i < data.length; i++){
+            if(info.email == data[i].email){
+                loginFlag = false;
+                break;
+            }
+        }
+
+        if(info.passwd != info.repasswd){
+            loginFlag = false;
+        }
+
+        if(loginFlag === true){
+            let user = {
+                email: info.email,
+                password: info.passwd,
+            };
+            data.push(user);
+            fs.writeFileSync("user.json",JSON.stringify(data));
+            res.send({status:"success"});
+            res.redirect('index');
+        }
+
+        if(loginFlag === false){
+
+            res.send({status:"fail", message:"邮箱已注册"});
+        }
+
     }
     else{
+        console.log(req.session.code);
         res.send({status:"fail"});
     }
 });
