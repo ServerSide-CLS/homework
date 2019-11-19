f2de5a1e6133d48dd906ebf45c032157c2c83efd
LDL12 <55759999+LDL12@users.noreply.github.com>
2019-11-19 22:27:32 +0800

    Delete var.js

--- 2017212212193/ex9/public/javascript/var.js
+++
@@ -1,5 +0,0 @@
-var e_mail = document.getElementById("e_mail");//获取邮箱input
-var pwd = document.getElementById("pwd");//获取密码input
-var pwd_again = document.getElementById("pwd_again");//获取确认密码input
-var VC = document.getElementById("VC");//获取验证码input
-var btn = document.getElementById("btn");//倒计时input
